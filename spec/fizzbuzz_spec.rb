b59e5909abbb51403429d654ccd3afda14f55ac0
JuglrdQuan Bessee <ikaquanbessee@yahoo.com>
2018-05-23 01:23:50 +0000

    automatically backed up by learn

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -23,3 +23,18 @@ describe "fizzbuzz" do
   end
 end
 
+def fizzbuzz(int)
+  if int % 3 == 0 && int % 5 == 0
+   return "FizzBuzz"
+  end
+
+  if int % 5 == 0
+   return "Buzz"
+  end
+
+  if int % 3 == 0
+   return "Fizz"
+  end
+
+end
+
